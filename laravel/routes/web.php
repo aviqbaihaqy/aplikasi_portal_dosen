f1a5bb7e22407e0aaf044c3e20be613b3261cad8
itmamAS <itmam2701@gmail.com>
2018-06-09 16:33:31 +0700

    berita acara upload

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/mahasiswa', 'MahasiswaController@index')->name('mahasiswa');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
